18c5bf1ec1b7f3aa9536d9d4458c9ff7b03edd4a
Tom Deneire <tomdeneire@gmail.com>
2022-08-16 14:11:55 +0200

    log test results

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 GOOS=js GOARCH=wasm go build -o main.wasm
 cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" .
-go test tomdeneire.github.io/ulpia/uxml
-go test tomdeneire.github.io/ulpia/ujson
+go test -json tomdeneire.github.io/ulpia/uxml > ./test/uxml_test.json
+go test -json tomdeneire.github.io/ulpia/ujson > ./test/ujson_test.json
